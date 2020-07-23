efc1290030a5f0dee0da6fbad416097a09268f5e
wahiddeveloper <68354730+wahiddeveloper@users.noreply.github.com>
2020-07-23 21:54:09 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         String questions = null;
+        
         Scanner input = new Scanner(System.in);
 
         Questions.questionsData(questions, input, Questions.getScore());
